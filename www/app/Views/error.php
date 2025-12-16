ddad8eaff2aa83254730f94240001a3e161c0331
XaquinAlves <xaquinalves@gmail.com>
2025-12-16 10:22:07 +0100

    Error view added for debug

--- www/app/Views/error.php
+++ www/app/Views/error.php
@@ -1,7 +1,24 @@
-<div class="row">  
+<div class="row">
     <div class="col-12">
         <div class="alert alert-danger">
-            <?php echo $texto; ?>
+            <?php
+            if (!empty($cabecera)) {
+                ?>
+                <h3><i class="fas fa-exclamation-triangle"></i> <?php echo $cabecera ?></h3>
+                <?php
+            }
+            ?>
+            <?php
+            if (!is_array($texto)) {
+                echo $texto;
+            }
+            else {
+                foreach ($texto as $value) {
+                    ?>
+                    <p class="mt-3"><strong><?php echo $value['titulo'] ?? '' ?></p><p></strong><?php echo $value['valor'] ?></p>
+                    <?php
+                }
+            }?>
         </div>
     </div>
 </div>
