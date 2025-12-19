3e5fd14223ca14e96e38d9095a0e3f268a1f2293
XaquinAlves <xaquinalves@gmail.com>
2025-12-19 11:27:13 +0100

    Doing Delete, Update

--- www/app/Views/categorias.view.php
+++ www/app/Views/categorias.view.php
@@ -112,8 +112,39 @@ declare(strict_types=1);
                                            class="btn btn-success btn-sm">
                                             <i class="fas fa-edit"></i>
                                         </a>
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-danger" data-toggle="modal"
+                                                data-target="#<?php echo $categoria['id_cat'] ?>borradoModal">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
                                     </td>
                                 </tr>
+                                <div class="modal fade" id="<?php echo $categoria['id_cat'] ?>borradoModal" tabindex="-1" role="dialog"
+                                     aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="<?php echo $categoria['id_cat']  ?>borradoModalLabel">
+                                                    ¿Estás seguro de borrar la categoría
+                                                    <?php echo $categoria['cat_name']  ?> ?
+                                                </h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                                                    Cancelar
+                                                </button>
+                                                <a href="/categorias/borrar/<?php echo $categoria['id_cat'] ?>"
+                                                   class="btn btn-danger">
+                                                    Borrar
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             <?php } ?>
                             </tbody>
                         </table>
